00f9b88939dd921fdc7467830bcf3b354a44337d
terry lee <2tk.java@gmail.com>
2022-02-07 14:10:17 +0900

    install vue-cli and learned difference vue-cli

---
+++ vue-cli/src/difference-cli.vue
@@ -0,0 +1,26 @@
+var appHeader = {
+    template: '<div>header</div>',
+    methods: {
+        addNum: function(){
+            
+        }
+    }
+}
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    //Javascript - 인스턴스 옵션
+    methods: {
+        addNum: function () {
+            
+        }
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>

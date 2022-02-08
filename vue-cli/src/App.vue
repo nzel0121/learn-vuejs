dce07c5d9fa39afe6d8046df46dda564cea9315f
terry lee <2tk.java@gmail.com>
2022-02-08 11:31:30 +0900

    learned event emit

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,7 +2,9 @@
   <div>
     {{ str }}
     <!-- <app-header v-bind:프롭스 속성 이름="상위컴포넌트의 데이터이름"></app-header> -->
-    <app-header v-bind:propsdata="str"></app-header>
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
@@ -17,6 +19,11 @@ export default {
   },
   components: {
     'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function(){
+      this.str = 'changed';
+    }
   }
 }
 </script>

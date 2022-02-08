dbec320b44305a571931b682274d45dce3eecc77
terry lee <2tk.java@gmail.com>
2022-02-08 14:25:25 +0900

    npm install axios

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,19 +1,33 @@
 <template>
-  <form action="">
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input type="text" id="username">
+      <input type="text" id="username" v-model="username">
     </div>
     <div>
       <label for="password">pw:</label>
-      <input type="password" id="password">
+      <input type="password" id="password" v-model="password">
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  data: function(){
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function(){
+      //event.preventDefault();
+      console.log(this.username, this.password);
+    }
+  }
 
 }
 </script>

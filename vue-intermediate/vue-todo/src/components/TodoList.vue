d07a988590f645676b72258d6a4982cdc6494a0a
terry lee <2tk.java@gmail.com>
2022-02-09 18:22:09 +0900

    add todolist component markup and fuction

--- vue-intermediate/vue-todo/src/components/TodoList.vue
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -1,15 +1,74 @@
 <template>
   <div>
-      list
+      <ul>
+         <li v-for="(todoItem,index) in todoItems" v-bind:key="todoItem" class="shadow">
+          {{todoItem}}
+          <span class="removeBtn" v-on:click="removeTodo(todoItem,index)">
+            <i class="fas fa-trash-alt"></i>
+          </span>
+         </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return {
+            todoItems: []
+        }
+    },
+    methods:{
+      removeTodo: function (todoItem, index) {
+        console.log('remove Items',todoItem, index);
+        localStorage.removeItem(localStorage.key(index));
+        this.todoItems.splice(index,1);
+      }
+    },
+    created: function(){
+        if(localStorage.length > 0){
+            for(var i=0;i< localStorage.length; i++){
+                // console.log(localStorage.key(i));
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+    }
 }
 </script>
 
 <style>
-
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBox {
+  line-height: 45px;
+  color: #72acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
+}
+.textCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn{
+  margin-left: auto;
+  color: #de4343;
+}
 </style>

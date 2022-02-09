0d5916a3e46e28afd23227b7b8abb5670ce506e1
terry lee <2tk.java@gmail.com>
2022-02-09 13:10:28 +0900

    create and register components

--- vue-intermediate/vue-todo/src/App.vue
+++ vue-intermediate/vue-todo/src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
-   
+   <TodoHeader/>
+   <TodoInput/>
+   <TodoList/>
+   <TodoFooter/>
   </div>
 </template>
 
 <script>
-
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  
+  components:{
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter
+  }
 }
 </script>
 

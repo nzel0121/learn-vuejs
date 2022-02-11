29ff29df771114255a5c3ed11bf239b96b05d493
terry lee <2tk.java@gmail.com>
2022-02-12 00:49:34 +0900

    Refactor , move lifecycle[created] TodoList to App[container]

--- vue-intermediate/vue-todo/src/App.vue
+++ vue-intermediate/vue-todo/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
    <TodoHeader/>
    <TodoInput/>
-   <TodoList/>
+   <TodoList v-bind:propsdata="todoItems"/>
    <TodoFooter/>
   </div>
 </template>
@@ -14,6 +14,21 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
+  data: function(){
+    return {
+      todoItems:[]
+    }
+  },
+  created: function(){
+    if(localStorage.length > 0){
+        for(var i=0;i< localStorage.length; i++){
+            // console.log(localStorage.key(i));
+            if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+              this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            }
+        }
+    }
+  },
   components:{
     'TodoHeader' : TodoHeader,
     'TodoInput' : TodoInput,
